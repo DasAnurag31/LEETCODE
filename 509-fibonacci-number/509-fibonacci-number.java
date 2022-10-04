a672b52da034283440dc17eabbb032b5d9e2e9b7
Anurag Das <dasanurag3129@gmail.com>
2022-10-04 11:14:59 +0530

    Time: 123 ms (5.93%), Space: 116.5 MB (5.50%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n) {
+        int[] dp=new int[n+1];
+        if(n==0 || n==1)
+        {
+           dp[n]=n;
+            return dp[n];
+        }
+        if(dp[n]!=0)
+        {
+            return dp[n];
+        }
+        dp[n]=fib(n-1)+fib(n-2);
+        return dp[n];
+    }
+}
