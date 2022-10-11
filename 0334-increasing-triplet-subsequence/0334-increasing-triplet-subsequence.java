6737e8913721411663f4ee07802040e3a753cdac
Anurag Das <dasanurag3129@gmail.com>
2022-10-11 22:14:33 +0530

    Time: 2 ms (99.05%), Space: 90.9 MB (78.41%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,36 +1,16 @@
 class Solution { 
     public boolean increasingTriplet(int[] nums) {
-       // for(int i=0;i<nums.length-2;i++)
-       // {
-       //     for(int j=i+1;j<nums.length-1;j++)
-       //     {
-       //         for(int k=j+1;k<nums.length;k++)
-       //         {
-       //             if(nums[k]> nums[j] && nums[j]>nums[i])
-       //                 return true;
-       //         }
-       //     }
-       // }
-       //  return false;
-        
-        // int start=0;
-        // int end=start+2;
-        // while(end<nums.length)
-        // {
-        //     if(nums[start]<nums[start+1] && nums[start+1]<nums[end])
-        //         return true;
-        //     start++;
-        //     end++;
-        // }
-        // return false;
-        
-         int max1 = Integer.MAX_VALUE;
-       int max2 = Integer.MAX_VALUE;
-       for(int n : nums) {
-           if(n <= max1) max1 = n;
-           else if(n <= max2) max2 = n;
-           else return true;
-       }
-       return false;
+        int smallest=Integer.MAX_VALUE;
+        int small=Integer.MAX_VALUE;
+        for(int n:nums)
+        {
+            if(n<=smallest)
+                smallest=n;
+            else if(n<=small)
+                small=n;
+            else
+                return true;
+        }
+        return false;
     }
 }
