3da94482b70263e9a988af127e318455d86d26c1
Anurag Das <dasanurag3129@gmail.com>
2022-10-23 09:25:43 +0530

    Time: 41 ms (9.69%), Space: 57.3 MB (8.83%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -4,13 +4,13 @@ class Solution {
         HashSet<Integer> set = new HashSet<>();
         
         // find duplicate using set
-        for(int i = 0; i < nums.length; ++i){
+        for(int i = 0; i < nums.length;i++){
             if(set.contains(nums[i]))   dup = nums[i];
             set.add(nums[i]);
         }
         
         // find missing
-        for(int i = 1; i <= nums.length; ++i){
+        for(int i = 1; i <= nums.length;i++){
             if(set.contains(i)== false){
                 miss = i;
                 break;
