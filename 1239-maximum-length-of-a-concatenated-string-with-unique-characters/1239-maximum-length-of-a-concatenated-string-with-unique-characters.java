b653da7509ffb551eda6a39fc94e1e64d7b890ba
Anurag Das <dasanurag3129@gmail.com>
2022-10-24 10:37:09 +0530

    Time: 641 ms (7.85%), Space: 116.8 MB (11.94%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,43 +1,25 @@
 class Solution {
-    public int maxLength(List<String> arr) {
-        return solve(arr, 0, "");
+    public int isApplicable(String res){
+        int[] cnt = new int[26];
+        for(int i = 0 ; i < res.length() ; i++)
+            cnt[res.charAt(i) - 'a']++;
+        for(int i = 0 ; i < 26 ; i++)
+            if(cnt[i] > 1) return 0;
+        return res.length();
     }
-    
-    
-    public int solve(List<String> arr, int i, String soFar)
-    {
-        if(i == arr.size())
-        {
-            if(isValid(soFar))
-            {
-                return soFar.length();
-            }
-            return 0;
+    public int solve(List<String> arr , int idx, String res){
+        if(idx == arr.size()){
+            return isApplicable(res);
         }
         
-        int size1 = solve(arr, i+1, soFar);
-        int size2 = solve(arr, i+1, soFar+arr.get(i));
-        
+        int len  = res.length();
+        res += (arr.get(idx));
+        int size1 = solve(arr , idx + 1 , res);
+        int size2 = solve(arr , idx + 1 , res.substring(0,len));
         return Math.max(size1, size2);
-
     }
     
-    
-    public boolean isValid(String s)
-    {
-        int[] freq = new int[26];
-        
-        for(int i=0;i<s.length();i++)
-        {
-            int val = s.charAt(i) - 'a';
-            freq[val]++;
-            
-            if(freq[val]>1)
-            {
-                return false;
-            }
-        }
-        
-        return true;
+    public int maxLength(List<String> arr) {
+        return solve(arr , 0 ,"");
     }
 }
